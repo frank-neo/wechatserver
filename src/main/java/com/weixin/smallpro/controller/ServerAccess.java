82b8058a5bbac2dc960e9aece443a6f387964c6b
franklee <839908968@qq.com>
2020-08-06 22:25:08 +0800

    123

--- src/main/java/com/weixin/smallpro/controller/ServerAccess.java
+++ src/main/java/com/weixin/smallpro/controller/ServerAccess.java
@@ -6,15 +6,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class ServerAccess {
 
-    @RequestMapping(value = "/wechataccess",method = RequestMethod.GET)
-    public String wechataccess(String signature,String timestamp,String nonce,String echostr){
-
-        System.out.println("signature ====>"+signature+",timestamp ====>"+timestamp+",nonce ====>"+nonce+",echostr ====>"+echostr);
-
+    @RequestMapping(value = "/wechataccess", method = RequestMethod.GET)
+    public String wechataccess(String signature, String timestamp, String nonce, String echostr) {
+        System.out.println("signature ====>" + signature +
+                ",timestamp ====>" + timestamp + ",nonce ====>"+",echostr ====>" + echostr);
         if (Sha1Unit.checkSignature(signature, timestamp, nonce)) {
-            System.out.print("校验通过echostr返回：=====>"+echostr); // 校验通过，原样返回echostr参数内容
+            System.out.print("校验通过echostr返回：=====>" + echostr); // 校验通过，原样返回echostr参数内容
             return echostr;
         }
         return "wrong ServerAccess!";
